f120f82edb517cdd734ccd0d7cc20c3c20a322e4
sobrienpdx <shaunna.obrien@gmail.com>
2018-11-06 17:27:08 -0800

    added empty template

--- coding_notes/settings.py
+++ coding_notes/settings.py
@@ -57,6 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
+		'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,3 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+	os.path.join(BASE_DIR, 'static'),
+	os.path.join(
+	os.path.dirname(__file__),
+	'static',
+	),
+)
--- notes_app/urls.py
+++ notes_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.conf.urls import url, include
 from rest_framework import routers
+from django.views.generic import TemplateView
 
 router = routers.DefaultRouter()
 router.register(r'notes', views.NoteViewSet)
@@ -16,5 +17,6 @@ urlpatterns = [
 	path('', views.test, name='test'),
 	path('api/', include(router.urls), name='api'),
 	# path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+	path('notes/', TemplateView.as_view(template_name='notes_app/notes.html'), name='notes'),
 
 ]
