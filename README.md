05795cdce08a5b87f8395628ec97b47b4d0a3f73
Daniel Miles <daniel.t.miles@gmail.com>
2018-12-06 14:57:11 -0800

    editing notes

---
+++ README.md
@@ -0,0 +1,3 @@
+notes for ourselves:
+
+When we left off, we figured out how to use patch to edit a note, and make sure we were editing the right note. We were on our way towards a "done" state but since we didn't have a visual on it, we edited the text instead.
--- notes_app/static/notes_app/notes.js
+++ notes_app/static/notes_app/notes.js
@@ -1,18 +1,33 @@
 var app = new Vue({
-  el: '#notes_list',
+  el: '#app',
   delimiters: ['${', '}'],
   data: {
     notes: "Hi",
+    new_note: "",
   },
   http: {
-    root: '/api/',
+    root: '',
     headers: {},
   },
   methods: {
     get_notes: function(){
-      this.$http.get('notes').then((response) => {
+      this.$http.get('/api/notes').then((response) => {
         this.notes = response.body;
       });
+    },
+    save_new_note: function(){
+      this.$http.post('/api/notes/', {text: this.new_note}).then((response) => {
+        this.get_notes();
+        this.new_note = "";
+      })
+    },
+    toggle_done: function(note_obj){
+      console.log("trying to update " + note_obj)
+      console.log("note url: " + note_obj.url)
+      this.$http.patch(note_obj.url, {text: "we totes edited this"}).then((response) => {
+        console.log("we got: " + response.body)
+        this.get_notes();
+      })
     },
   },
   mounted: function(){
