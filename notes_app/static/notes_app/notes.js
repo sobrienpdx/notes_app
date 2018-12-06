26beab7bfba556d4fad20839d72497e5404f161f
Daniel Miles <daniel.t.miles@gmail.com>
2018-12-06 14:03:16 -0800

    making vue UI fetch and display notes

--- notes_app/static/notes_app/notes.js
+++ notes_app/static/notes_app/notes.js
@@ -1,3 +1,21 @@
 var app = new Vue({
+  el: '#notes_list',
   delimiters: ['${', '}'],
+  data: {
+    notes: "Hi",
+  },
+  http: {
+    root: '/api/',
+    headers: {},
+  },
+  methods: {
+    get_notes: function(){
+      this.$http.get('notes').then((response) => {
+        this.notes = response.body;
+      });
+    },
+  },
+  mounted: function(){
+    this.get_notes();
+  },
 })
--- notes_app/urls.py
+++ notes_app/urls.py
@@ -7,16 +7,12 @@ from django.views.generic import TemplateView
 router = routers.DefaultRouter()
 router.register(r'notes', views.NoteViewSet)
 
-
-
-app_name = 'notes_app' # for namespacing
-
-
+app_name = 'notes_app'  # for namespacing
 
 urlpatterns = [
-	path('', views.test, name='test'),
-	path('api/', include(router.urls), name='api'),
-	# path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-	path('notes/', TemplateView.as_view(template_name='notes_app/notes.html'), name='notes'),
+    #path('', views.test, name='test'),
+    path('api/', include(router.urls), name='api'),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', TemplateView.as_view(template_name='notes_app/notes.html'), name='notes'),
 
 ]
